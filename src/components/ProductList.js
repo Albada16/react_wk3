326a3acec7a7e6983d3639d0d00145776eb54dc4
Albada16 <NamelessOAO@gmail.com>
2022-10-01 21:07:49 +0800

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -7,10 +7,10 @@ export default function ProductList({products}) {
     {products.map(product => (
         <Col 
           key={product.id} 
-          sm={{ span: 12 }} 
-          lg={{ span: 8 }}
-          xl={{ span: 6 }}
-          xxl={{ span: 4 }}
+          sm={{ span: 14 }} 
+          lg={{ span: 10 }}
+          xl={{ span: 8 }}
+          xxl={{ span: 6 }}
         >
           <ProductItem product={product}/>
         </Col>
